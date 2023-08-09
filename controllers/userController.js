e7482705c9781fa92edc9530500e9c3c499a4a10
AMillsy <00adammills00@gmail.com>
2023-08-09 16:05:57 +0100

    fixing findAllUser route

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,13 +3,15 @@ const { User, Thought } = require("../models");
 module.exports = {
   findAllUsers: async function (req, res) {
     try {
-      const allUsers = User.find({});
+      console.log("I hit this route");
+      const allUsers = await User.find({}).select("-id");
 
       if (!allUsers) return res.status(404).json({ message: "No users found" });
 
+      console.log("Are we getting here?");
       res.status(200).json(allUsers);
     } catch (error) {
-      res.status(400).json(error);
+      res.status(400).json({ message: "An error has occured" });
     }
   },
 };
--- models/User.js
+++ models/User.js
@@ -24,10 +24,12 @@ const userSchema = new Schema(
       type: Schema.Types.ObjectId,
       ref: "thought",
     },
-    friends: {
-      type: Schema.Types.ObjectId,
-      ref: "user",
-    },
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
   },
   {
     toJSON: {
