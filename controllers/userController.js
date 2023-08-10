b34b2cbed16c3f002768a45ab79b2551d46c095a
AMillsy <00adammills00@gmail.com>
2023-08-10 16:59:06 +0100

    can create new users

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,7 +15,7 @@ module.exports = {
   findSingleUser: async function (req, res) {
     try {
       const findUser = await User.findById(req.params.id)
-        .select("-_id")
+        .select(["-_id", "-id"])
         .populate("thoughts");
 
       if (!findUser) res.status(404).json({ message: "No user found" });
@@ -24,6 +24,17 @@ module.exports = {
     } catch (error) {
       console.log(error);
       res.status(404).json(error);
+    }
+  },
+
+  createNewUser: async function (req, res) {
+    try {
+      const newUser = await User.create(req.body);
+
+      res.status(200).json(newUser);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error);
     }
   },
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,8 +3,9 @@ const router = require("express").Router();
 const {
   findAllUsers,
   findSingleUser,
+  createNewUser,
 } = require("../../controllers/userController");
 
-router.get("/", findAllUsers);
+router.route("/").get(findAllUsers).post(createNewUser);
 router.get("/:id", findSingleUser);
 module.exports = router;
