6e5ff03c72286fc8f46adb6e5aafd58ab93f6b0e
AMillsy <00adammills00@gmail.com>
2023-08-10 16:50:09 +0100

    can find a single user

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,14 +2,28 @@ const { User, Thought } = require("../models");
 
 module.exports = {
   findAllUsers: async function (req, res) {
-    // try {
-    const allUsers = await User.find({}).select("-id").populate("thoughts");
+    try {
+      const allUsers = await User.find({}).populate("thoughts");
 
-    if (!allUsers) return res.status(404).json({ message: "No users found" });
+      if (!allUsers) return res.status(404).json({ message: "No users found" });
 
-    res.status(200).json(allUsers);
-    // } catch (error) {
-    //   res.status(400).json({ message: "An error has occured" });
-    // }
+      res.status(200).json(allUsers);
+    } catch (error) {
+      res.status(400).json({ message: "An error has occured" });
+    }
+  },
+  findSingleUser: async function (req, res) {
+    try {
+      const findUser = await User.findById(req.params.id)
+        .select("-_id")
+        .populate("thoughts");
+
+      if (!findUser) res.status(404).json({ message: "No user found" });
+
+      res.status(200).json(findUser);
+    } catch (error) {
+      console.log(error);
+      res.status(404).json(error);
+    }
   },
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
 
-const { findAllUsers } = require("../../controllers/userController");
+const {
+  findAllUsers,
+  findSingleUser,
+} = require("../../controllers/userController");
 
 router.get("/", findAllUsers);
-
+router.get("/:id", findSingleUser);
 module.exports = router;
