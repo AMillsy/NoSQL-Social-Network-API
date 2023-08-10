7bf51fe5251ff9a230947738935cc3249ed1a83b
AMillsy <00adammills00@gmail.com>
2023-08-10 17:42:00 +0100

    find all, one and create new thoughts

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,4 +1,56 @@
 const { ObjectId } = require("mongoose").Types;
 const { Thought, User } = require("../models");
 
-module.exports = {};
+module.exports = {
+  getAllThoughts: async function (req, res) {
+    try {
+      const allThoughts = await Thought.find({});
+
+      if (!allThoughts)
+        return res.status(404).json({ message: "No thoughts found" });
+
+      res.status(200).json(allThoughts);
+    } catch (error) {
+      res.status(404).json(error);
+    }
+  },
+  getSingleThought: async function (req, res) {
+    try {
+      const thought = await Thought.findById(req.params.id);
+
+      if (!thought)
+        return res.status(404).json({ message: "No thought found" });
+
+      res.status(200).json(thought);
+    } catch (error) {
+      res.status(404).json(error);
+    }
+  },
+  createThought: async function (req, res) {
+    try {
+      const createdThought = await Thought.create(req.body);
+
+      const user = req.body.username;
+
+      const updateUser = await User.findOneAndUpdate(
+        { username: user },
+        {
+          $addToSet: { thoughts: new ObjectId(createdThought.insertedId) },
+        },
+        { new: true }
+      ).populate("thoughts");
+
+      if (!updateUser)
+        return res
+          .status(404)
+          .json({ message: "Thought created but no user found" });
+
+      res.status(200).json({
+        message: "Thought created and added to user",
+        user: updatedUser,
+      });
+    } catch (error) {
+      res.status(404).json(error);
+    }
+  },
+};
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,3 +1,11 @@
 const router = require("express").Router();
+const {
+  getAllThoughts,
+  getSingleThought,
+  createThought,
+} = require("../../controllers/thoughtController");
+
+router.route("/").get(getAllThoughts).post(createThought);
+router.route("/:id").get(getSingleThought);
 
 module.exports = router;
